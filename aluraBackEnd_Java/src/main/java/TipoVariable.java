31c59ab8c097a8c027cf367c2663f79df2abd39f
Kevin Polanco <kevinpolanco12@gmail.com>
2023-02-13 15:42:05 -0500

    Getting started with Alura Back End

---
+++ aluraBackEnd_Java/src/main/java/TipoVariable.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TipoVariable {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please enter a number:\n");
+        
+        double number = input.nextInt();
+        
+        System.out.printf("%s%.0f","Your number is ",number);
+        
+        
+        
+        
+    }//End Main
+    
+}//End class
