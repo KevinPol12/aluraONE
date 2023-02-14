86aa71f95bf6c3b1d3f2894bdd84300594c5ca44
Kevin Polanco <kevinpolanco12@gmail.com>
2023-02-14 18:40:35 -0500

    Getting started with Object Orientation in Java with the project bytebank

--- aluraBackEnd_Java/src/main/java/TipoVariable.java
+++ aluraBackEnd_Java/src/main/java/TipoVariable.java
@@ -1,20 +1,24 @@
+
 import java.util.Scanner;
 
 public class TipoVariable {
-    
+
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
-        
-        System.out.println("Please enter a number:\n");
-        
-        double number = input.nextInt();
-        
-        System.out.printf("%s%.0f","Your number is ",number);
-        
-        
-        
-        
+
+        int num = 1;
+
+        do {
+
+            System.out.println("Please enter a number:");
+
+            double number = input.nextInt();
+
+            System.out.printf("%s%.0f%n%n", "Your number is ", number);
+            num++;
+        } while (num <= 2);
+
     }//End Main
-    
+
 }//End class
